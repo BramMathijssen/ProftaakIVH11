e797e78f11ec1406104fd770f8c53996884a8a05
Bram Mathijssen <b.mathijssen@student.avans.nl>
2019-05-19 18:05:00 +0200

    removed //@OneToMany(mappedBy = "mealCook") from Student so a student can be used multiple times on meals

--- src/main/java/avans/ivh11/proftaak/Domain/Student.java
+++ src/main/java/avans/ivh11/proftaak/Domain/Student.java
@@ -2,10 +2,7 @@ package avans.ivh11.proftaak.Domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.util.Calendar;
 
 @Entity
@@ -19,8 +16,11 @@ public class Student {
 
     private Calendar created = Calendar.getInstance();
 
-    @OneToOne(mappedBy = "mealCook")
+    //@OneToMany(mappedBy = "mealCook")
+    //displays meal_id NULL in the database.
+    //can implement a list of meals with a @OneToMany relationship to couple multiple meals to 1 student to check in the view.
     @JsonBackReference
+    @ManyToOne
     private Meal meal;
 
 
